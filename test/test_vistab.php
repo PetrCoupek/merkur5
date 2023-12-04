02e8189f5e51735df6867fb6c40791ddf8bf56d8
PetrCoupek <petr.coupek@geology.cz>
2023-12-04 14:29:20 +0100

    041223b

--- test/test_vistab.php
+++ test/test_vistab.php
@@ -5,14 +5,14 @@
  */
 
 include_once '../lib/mlib.php';
-//include_once '../lib/mbt.php';
+include_once 'ini.php';
 //include_once '../lib/vistab.php';
 
 M5::set('header','Test Vistab');
 M5::set('debug',true);
 M5::skeleton('../');
 
-$db = new OpenDB_Oracle('dsn=sdedb02;uid=app_dkb;pwd=jsdn*6343Jkjsedn*324');
+$db = new OpenDB_Oracle(CONN_APP_DKB_02);
 //deb($db->Pragma("table_info('dat_lok1.lok')") );
 $tt= new VisTab(
   ['sprikaz'=>"select id, nazev, ochrana_stup_kod, ochrana_kat_kod, ochrana_dop from dat_lok1.lok", 
